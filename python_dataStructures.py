c074a972078db5ffadfbb76e8dba550d969f8563
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2025-07-06 15:02:38 -0500

    Update python_dataStructures.py add tuples

--- python_dataStructures.py
+++ python_dataStructures.py
@@ -36,3 +36,12 @@ print(grades)
 
 nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(nested_list[1][2])
+
+#Tuples - Immutable (cannot be changed) - ()
+coordinates = (40.7128, 74.0060) #New York
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+student = ("John Doe", 8675309, "Computer Science")
+
+print(student[1])
